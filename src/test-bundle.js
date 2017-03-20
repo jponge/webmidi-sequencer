5ed3ddf2b6f72a449eaf6e0219e2d39f79ca2640
Julien Ponge <julien.ponge@insa-lyon.fr>
2017-03-20 12:13:26 +0100

    Some sequences

--- src/test-bundle.js
+++ src/test-bundle.js
@@ -7,16 +7,104 @@ midiSequencer.whenMidiReady(() => {
   let traktorIn = midiSequencer.inputPort(port => port.name.includes("Traktor Virtual"))
   let traktorOut = midiSequencer.outputPort(port => port.name.includes("Traktor Virtual"))
 
-  let sequencer = new midiSequencer.Sequencer(traktorIn, traktorOut, 2, 4, [
+  // Kick C1
+  // Clap D1
+  // Snare F1
+  // Shaker E1
+
+  const seq1 = [
+    // ---- //
     [["C1"], 1],
-    [["E1"], 1],
-    [["C1", "D1", "F1"], 1],
-    [["E1", "F1"], 1],
+    null,
+    null,
+    null,
+    // ---- //
     [["C1"], 1],
-    [["E1"], 1],
+    null,
+    null,
+    null,
+    // ---- //
+    [["C1"], 1],
+    null,
+    null,
+    null,
+    // ---- //
+    [["C1"], 1],
+    null,
+    null,
+    null,
+  ]
+
+  const seq2 = [
+    // ---- //
+    [["C1"], 1],
+    null,
+    null,
+    null,
+    // ---- //
     [["C1", "D1"], 1],
+    null,
+    null,
+    null,
+    // ---- //
+    [["C1"], 1],
+    null,
+    null,
+    null,
+    // ---- //
+    [["C1", "D1"], 1],
+    null,
+    null,
+    [["D1"], 1],
+  ]
+
+  const seq3 = [
+    // ---- //
+    [["C1"], 1],
+    null,
     [["E1"], 1],
-  ])
+    null,
+    // ---- //
+    [["C1", "D1"], 1],
+    null,
+    [["E1"], 1],
+    null,
+    // ---- //
+    [["C1"], 1],
+    null,
+    [["E1"], 1],
+    null,
+    // ---- //
+    [["C1", "D1"], 1],
+    null,
+    [["E1"], 1],
+    [["D1"], 1],
+  ]
+
+  const seq4 = [
+    // ---- //
+    [["C1"], 1],
+    null,
+    [["E1"], 1],
+    null,
+    // ---- //
+    [["C1", "D1"], 1],
+    null,
+    [["E1"], 1],
+    [["F1"], 1],
+    // ---- //
+    [["C1"], 1],
+    null,
+    [["E1", "F1"], 1],
+    [["E1"], 1],
+    // ---- //
+    [["C1", "D1", "F1"], 1],
+    [["F1"], 1],
+    [["E1", "D1"], 1],
+    [["D1", "E1"], 1],
+  ]
+
+  let sequencer = new midiSequencer.Sequencer(traktorIn, traktorOut, 4, 4, seq4)
 
   sequencer.addListener((event) => {
     console.dir(event)
